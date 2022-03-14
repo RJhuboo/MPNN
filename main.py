15950e59f5867e0e72ba304f096dcfa4a709abd5
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-03-14 19:07:56 +0100

    Main correction

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import argparse
 from sklearn.model_selection import KFold
-
+from torch.utils.data import Dataset, DataLoader
 import random
 #import wandb
 
@@ -15,15 +15,15 @@ import dataloader
 
 # GPU or CPU
 if torch.cuda.is_available():  
-  dev = "cuda:0" 
+  device = "cuda:0" 
 else:  
-  dev = "cpu"
+  device = "cpu"
   
 ''' Options '''
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--label_dir", default = "/media/rehan/Seagate Expansion Drive/sainbiose/New_datasets/HR_TIF_NOAUGMENTATION/Labels_norm_aug.csv", help = "path to label csv file")
-parser.add_argument("--image_dir", default = "/media/rehan/Seagate Expansion Drive/sainbiose/New_datasets/HR_PNG_AUGMENTATION", help = "path to image directory")
+parser.add_argument("--label_dir", default = "./Label.csv", help = "path to label csv file")
+parser.add_argument("--image_dir", default = "./HR_trab", help = "path to image directory")
 parser.add_argument("--batch_size", default = 16, help = "number of batch")
 parser.add_argument("--nof", default = 16, help = "number of filter")
 parser.add_argument("--lr",default = 0.001, help = "learning rate")
@@ -48,33 +48,21 @@ else:
     datasets = dataloader.Datasets(image_dir = opt.image_dir)
 # defining the model
 model = Model.Net()
+model.to(device)
 
-# DATA SPLITING - Two possibilities : split data randomly or split data with specific index 
 if opt.mode == "Train" or opt.mode == "Test":
+    kf = KFold(n_splits = opt.k_fold, shuffle=True)
+    kf.get_n_splits(datasets)
+    for train_index, test_index in kf.split(datasets):
+    print("Train:", train_index[1:4],"Test:",test_index[1:4])
     nb_data = len(datasets)
-    nb_test = nb_data*(PERCENTAGE_TEST/100)
-    nb_dataset = nb_data - nb_test
-    # train,test = torch.utils.data.random_split(datasets, [int(nb_dataset),int(nb_test)]) # split data randomly
-    ids = np.array(range(0,nb_data))
-    random.shuffle(ids)
-    train_ids = ids[0:int(nb_dataset)]
-    test_ids = ids [int(nb_dataset):]
-    trainloader = DataLoader(datasets, batch_size = opt.batch_size, sampler = train_ids,  num_workers = 0 ) # Create batches and tensors
-    testloader = DataLoader(datasets, batch_size = 1, sampler = test_ids, num_workers = 0 ) # Create batches and tensors
-    # trainloader = DataLoader(train, batch_size = opt.batch_size, shuffle = True, num_workers = 0 ) # Create batches and tensors
-    # testloader = DataLoader(test, batch_size = 1, shuffle = False, num_workers = 0 ) # Create batches and tensors
-else:
-    testloader = DataLoader(datasets,batch_size = 1, num_workers =0)
-# training 
-
-if opt.mode == "Train" or opt.mode == "Test":
-    #wandb.init(entity='jhuboo', project='BPNN1')
-    #wandb.watch(model, log='all')
-    t = trainer.Trainer(opt,model)
+    trainloader = DataLoader(datasets, batch_size = opt.batch_size, sampler = train_index,  num_workers = 0 )
+    testloader =DataLoader(datasets, batch_size = 1, sampler = test_index, num_workers = 0 )
+    trainloader.to(device)
+    testloader.to(device)
+    t = Trainer(opt,model)
     for epoch in range(opt.nb_epochs):
         t.train(trainloader,epoch)
-        t.test(testloader,epoch)    
-    #wandb.finish()
+        t.test(testloader,epoch)
 else:
-    t = trainer.Trainer(opt,model)
-    t.test(testloader,4)
+    testloader = DataLoader(datasets,batch_size = 1, num_workers =0)
