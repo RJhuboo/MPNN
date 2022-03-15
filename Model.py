34e76979fdae08f0fad9189fe1812204c8592b6c
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-03-15 16:57:27 +0100

    Unet + parameters of models
    I add a Unet model which goes from 64 to 32 and back to 64 before entering in the neural network. I also add the neural network class to use it in every other model. I added some parameters to tune such as n1, n2, n3  for the numbers of neurons in the neural network. I added one layer in the Neural Network.

--- Model.py
+++ Model.py
@@ -3,28 +3,136 @@ from torchvision.utils import make_grid
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Net(nn.Module):
-    def __init__(self,n_f,n_l):
-        super(Net,self).__init__()
+ ## Neural Network for regression ##
+class NeuralNet(nn.Module):
+    def __init__(self,n1,n2,n3,out_channels):
+        super().__init__()
+        self.fc1 = nn.Linear(64**3,n1)
+        self.fc2 = nn.Linear(n1,n2)
+        self.fc3 = nn.Linear(n2,n3)
+        self.fc3 = nn.Linear(n3,out_channels)
+    def forward(self,x):
+        x = torch.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+                  
+## 3 CNN model ##
+class ConvNet(nn.Module):
+    def __init__(self,features,out_channels,n1=240,n2=120,n3=60):
+        super(ConvNet,self).__init__()
         # initialize CNN layers
+        for 
         self.conv1 = nn.Conv2d(1,n_f,kernel_size = 3,stride = 1, padding = 1)
         self.conv2 = nn.Conv2d(n_f,n_f*2, kernel_size = 3, stride = 1, padding = 1)
-        self.conv3 = nn.Conv2d(n_f*2,n_f*4, kernel_size = 3, stride = 1, padding = 1)
+        self.conv3 = nn.Conv2d(n_f*2,64, kernel_size = 3, stride = 1, padding = 1)
         self.pool = nn.MaxPool2d(2,2)
         # initialize NN layers
-        self.fc1 = nn.Linear(64**3,240)
-        self.fc2 = nn.Linear(240,120)
-        self.fc3 = nn.Linear(120,14)
+        self.neural = NeuralNet(n1,n2,n3,out_channels)
         # dropout
         # self.dropout = nn.Dropout(0.25)
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
-        x = torch.flatten(x,1)
-        # x = self.dropout(x)
-        x = F.relu(self.fc1(x))
-        # x = self.dropout(x)
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.neural(x)
         return x 
+    
+## UNET model ##
+class UNet(nn.Module):
+
+    def __init__(self, in_channels=1, out_channels=1,nb_label=14, n1,n2,n3, init_features=64):
+        super(UNet, self).__init__()
+
+        features = init_features
+        self.encoder1 = UNet._block(in_channels, features, name="enc1")
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.encoder2 = UNet._block(features, features * 2, name="enc2")
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.encoder3 = UNet._block(features * 2, features * 4, name="enc3")
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.encoder4 = UNet._block(features * 4, features * 8, name="enc4")
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.bottleneck = UNet._block(features * 8, features * 16, name="bottleneck")
+
+        self.upconv4 = nn.ConvTranspose2d(
+            features * 16, features * 8, kernel_size=2, stride=2
+        )
+        self.decoder4 = UNet._block((features * 8) * 2, features * 8, name="dec4")
+        self.upconv3 = nn.ConvTranspose2d(
+            features * 8, features * 4, kernel_size=2, stride=2
+        )
+        self.decoder3 = UNet._block((features * 4) * 2, features * 4, name="dec3")
+        self.upconv2 = nn.ConvTranspose2d(
+            features * 4, features * 2, kernel_size=2, stride=2
+        )
+        self.decoder2 = UNet._block((features * 2) * 2, features * 2, name="dec2")
+        self.upconv1 = nn.ConvTranspose2d(
+            features * 2, features, kernel_size=2, stride=2
+        )
+        self.decoder1 = UNet._block(features * 2, features, name="dec1")
+
+        self.conv = nn.Conv2d(
+            in_channels=features, out_channels=out_channels, kernel_size=1
+        )
+        
+        self.neural = NeuralNet(n1,n2,n3,nb_label)
+        
+    def forward(self, x):
+        enc1 = self.encoder1(x)
+        enc2 = self.encoder2(self.pool1(enc1))
+        enc3 = self.encoder3(self.pool2(enc2))
+        enc4 = self.encoder4(self.pool3(enc3))
+
+        bottleneck = self.bottleneck(self.pool4(enc4))
+
+        dec4 = self.upconv4(bottleneck)
+        dec4 = torch.cat((dec4, enc4), dim=1)
+        dec4 = self.decoder4(dec4)
+        dec3 = self.upconv3(dec4)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.decoder3(dec3)
+        dec2 = self.upconv2(dec3)
+        dec2 = torch.cat((dec2, enc2), dim=1)
+        dec2 = self.decoder2(dec2)
+        dec1 = self.upconv1(dec2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.decoder1(dec1)
+        return self.neural(dec1)
+
+    @staticmethod
+    def _block(in_channels, features, name):
+        return nn.Sequential(
+            OrderedDict(
+                [
+                    (
+                        name + "conv1",
+                        nn.Conv2d(
+                            in_channels=in_channels,
+                            out_channels=features,
+                            kernel_size=3,
+                            padding=1,
+                            bias=False,
+                        ),
+                    ),
+                    (name + "norm1", nn.BatchNorm2d(num_features=features)),
+                    (name + "relu1", nn.ReLU(inplace=True)),
+                    (
+                        name + "conv2",
+                        nn.Conv2d(
+                            in_channels=features,
+                            out_channels=features,
+                            kernel_size=3,
+                            padding=1,
+                            bias=False,
+                        ),
+                    ),
+                    (name + "norm2", nn.BatchNorm2d(num_features=features)),
+                    (name + "relu2", nn.ReLU(inplace=True)),
+                ]
+            )
+        )
+    
