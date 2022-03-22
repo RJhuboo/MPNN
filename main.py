00b5b34e279bf4dedec66f1015c234a5be20a398
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-03-22 12:24:25 +0100

    Create 2 functions

--- main.py
+++ main.py
@@ -51,6 +51,8 @@ NB_LABEL = 14
 
 '''functions'''
 
+## RESET WEIGHT FOR CROSS VALIDATION
+
 def reset_weights(m):
   '''
     Try resetting model weights to avoid
@@ -60,73 +62,131 @@ def reset_weights(m):
    if hasattr(layer, 'reset_parameters'):
     print(f'Reset trainable parameters of layer = {layer}')
     layer.reset_parameters()
+    
+### FOR CROSS VALIDATION
+
+def cross_validation():
+  # Create the folder where to save results and checkpoints
+  i=1
+  if os.path.isdir("./result/cross"+str(i)) == False:
+      save_folder = "./result/cross"+str(i)
+      os.mkdir(save_folder)
+  while True:
+      i += 1
+      if os.path.isdir("./result/cross"+str(i)) == False:
+          save_folder = "./result/cross"+str(i)
+          os.mkdir(save_folder)
+          break
+
+  datasets = dataloader.Datasets(csv_file = opt.label_dir, image_dir = opt.image_dir, opt=opt) # Create dataset
+  split = train_test_split(datasets,test_size = 0.2,random_state =1)
+  datasets = split[0]
+  if opt.norm_method == "standardization":
+      scaler = dataloader.standardization(opt.label_dir)
+  kf = KFold(n_splits = opt.k_fold, shuffle=True)
+  kf.get_n_splits(datasets)
+  score_train = []
+  score_test = []
+  score_mse_t = []
+  score_mse_v = []
+  print("start cross validation")
+  for train_index, test_index in kf.split(datasets):
+      print("Train:", train_index[1:4],"Test:",test_index[1:4])
+      trainloader = DataLoader(datasets, batch_size = opt.batch_size, sampler = train_index,  num_workers = opt.nb_workers )
+      testloader =DataLoader(datasets, batch_size = 1, sampler = test_index, num_workers = opt.nb_workers )
+
+      # defining the model
+      if opt.model == "ConvNet":
+          print("## Choose model : convnet ##")
+          model = Model.ConvNet(features =opt.nof,out_channels=NB_LABEL,k1 = 3,k2 = 3,k3= 3).to(device)
+      elif opt.model == "resnet50":
+          print("## Choose model : resnet50 ##")
+          model = Model.ResNet50(14,1).to(device)
+      elif opt.model == "restnet101":
+          print("## Choose model : resnet101 ##")
+          model = Model.ResNet101(14,1).to(device)
+      else:
+          print("## Choose model : Unet ##")
+          model = Model.UNet(in_channels=1,out_channels=1,nb_label=NB_LABEL, n1=opt.n1, n2=opt.n2, n3=opt.n3, init_features=opt.nof).to(device)
+          model.apply(reset_weights)
+      t = Trainer(opt,model,device,save_folder,scaler)
+      for epoch in range(opt.nb_epochs):
+          [r2_train,mse_train] = t.train(trainloader,epoch)
+          [r2_test,mse_test] = t.test(testloader,epoch)
+          score_train.append(r2_train)
+          score_test.append(r2_test)
+          score_mse_t.append(mse_train)
+          score_mse_v.append(mse_test)
+  with open(os.path.join(save_folder,opt.train_cross),'wb') as f:
+      pickle.dump(score_train, f)
+      pickle.dump(score_test, f)
+      pickle.dump(score_mse_t, f)
+      pickle.dump(sscore_mse_v, f)
+  #with open(os.path.join(save_folder,opt.test_cross),'wb') as f:
+  #    pickle.dump(score_test,f)
+  with open(os.path.join(save_folder,"history.txt"),'wb') as g:
+      history = "nof: " + str(opt.nof) + " model:" +str(opt.model) + " lr:" + str(opt.lr) + " neurons: " + str(opt.n1) + " " + str(opt.n2) + " " + str(opt.n3) + " kernel:" + str(3) + " norm data: " + str(opt.norm_method)
+      pickle.dump(history,g)
+  
+## FOR TRAINING
+
+def train():
+  # Create the folder where to save results and checkpoints
+  i=1
+  if os.path.isdir("./result/train"+str(i)) == False:
+      save_folder = "./result/train"+str(i)
+      os.mkdir(save_folder)
+  while True:
+      i += 1
+      if os.path.isdir("./result/train"+str(i)) == False:
+          save_folder = "./result/train"+str(i)
+          os.mkdir(save_folder)
+          break
+
+  # defining data
+  datasets = dataloader.Datasets(csv_file = opt.label_dir, image_dir = opt.image_dir) # Create dataset
+  split = train_test_split(data,test_size = 0.2,random_state =1)
+  print("start training")
+  trainloader = DataLoader(split[0], batch_size = opt.batch_size, num_workers = opt.nb_workers )
+  testloader =DataLoader(split[1], batch_size = 1, num_workers = opt.nb_workers )
+  # defining the model
+  if opt.model == "ConvNet":
+      print("## Choose model : convnet ##")
+      model = Model.ConvNet(features =opt.nof,out_channels=NB_LABEL,k1 = 3,k2 = 3,k3= 3).to(device)
+  elif opt.model == "resnet50":
+      print("## Choose model : resnet50 ##")
+      model = Model.ResNet50(14,1).to(device)
+  elif opt.model == "restnet101":
+      print("## Choose model : resnet101 ##")
+      model = Model.ResNet101(14,1).to(device)
+  else:
+      print("## Choose model : Unet ##")
+      model = Model.UNet(in_channels=1,out_channels=1,nb_label=NB_LABEL, n1=opt.n1, n2=opt.n2, n3=opt.n3, init_features=opt.nof).to(device)
+      model.apply(reset_weights)
+  t = Trainer(opt,model,device,save_folder,scaler)
+  for epoch in range(opt.nb_epochs):
+      [r2_train,mse_train] = t.train(trainloader,epoch)
+      [r2_test,mse_test] = t.test(testloader,epoch)
+      score_train.append(r2_train)
+      score_test.append(r2_test)
+      score_mse_t.append(mse_train)
+      score_mse_v.append(mse_test)
+  with open(os.path.join(save_folder,opt.train_cross),'wb') as f:
+      pickle.dump(score_train, f)
+      pickle.dump(score_test, f)
+      pickle.dump(score_mse_t, f)
+      pickle.dump(sscore_mse_v, f)
+  #with open(os.path.join(save_folder,opt.test_cross),'wb') as f:
+  #    pickle.dump(score_test,f)
+  with open(os.path.join(save_folder,"history.txt"),'wb') as g:
+      history = "nof: " + str(opt.nof) + " model:" +str(opt.model) + " lr:" + str(opt.lr) + " neurons: " + str(opt.n1) + " " + str(opt.n2) + " " + str(opt.n3) + " kernel:" + str(3) + " norm data: " + str(opt.norm_method)
+      pickle.dump(history,g)
+      
 
 ''' main '''
-# Create the folder where to save results and checkpoints
-i=1
-if os.path.isdir("./result/"+str(i)) == False:
-    save_folder = "./result/"+str(i)
-    os.mkdir(save_folder)
-while True:
-    i += 1
-    if os.path.isdir("./result/"+str(i)) == False:
-        save_folder = "./result/"+str(i)
-        os.mkdir(save_folder)
-        break
 
-# defining data
-if opt.mode == "Train" or opt.mode == "Test":
-    datasets = dataloader.Datasets(csv_file = opt.label_dir, image_dir = opt.image_dir, opt=opt) # Create dataset
-else:
-    datasets = dataloader.Test_Datasets(image_dir = opt.image_dir)
-if opt.mode == "Train" or opt.mode == "Test":
-    if opt.norm_method == "standardization":
-        scaler = dataloader.standardization(opt.label_dir)
-    kf = KFold(n_splits = opt.k_fold, shuffle=True)
-    kf.get_n_splits(datasets)
-    score_train = []
-    score_test = []
-    score_mse_t = []
-    score_mse_v = []
-    print("start cross validation")
-    for train_index, test_index in kf.split(datasets):
-        print("Train:", train_index[1:4],"Test:",test_index[1:4])
-        nb_data = len(datasets)
-        trainloader = DataLoader(datasets, batch_size = opt.batch_size, sampler = train_index,  num_workers = opt.nb_workers )
-        testloader =DataLoader(datasets, batch_size = 1, sampler = test_index, num_workers = opt.nb_workers )
-        
-        # defining the model
-        if opt.model == "ConvNet":
-            print("## Choose model : convnet ##")
-            model = Model.ConvNet(features =opt.nof,out_channels=NB_LABEL,k1 = 3,k2 = 3,k3= 3).to(device)
-        elif opt.model == "resnet50":
-            print("## Choose model : resnet50 ##")
-            model = Model.ResNet50(14,1).to(device)
-        elif opt.model == "restnet101":
-            print("## Choose model : resnet101 ##")
-            model = Model.ResNet101(14,1).to(device)
-        else:
-            print("## Choose model : Unet ##")
-            model = Model.UNet(in_channels=1,out_channels=1,nb_label=NB_LABEL, n1=opt.n1, n2=opt.n2, n3=opt.n3, init_features=opt.nof).to(device)
-            model.apply(reset_weights)
-        t = Trainer(opt,model,device,save_folder,scaler)
-        for epoch in range(opt.nb_epochs):
-            [r2_train,mse_train] = t.train(trainloader,epoch)
-            [r2_test,mse_test] = t.test(testloader,epoch)
-            score_train.append(r2_train)
-            score_test.append(r2_test)
-            score_mse_t.append(mse_train)
-            score_mse_v.append(mse_test)
-    with open(os.path.join(save_folder,opt.train_cross),'wb') as f:
-        pickle.dump(score_train, f)
-        pickle.dump(score_test, f)
-        pickle.dump(score_mse_t, f)
-        pickle.dump(sscore_mse_v, f)
-    #with open(os.path.join(save_folder,opt.test_cross),'wb') as f:
-    #    pickle.dump(score_test,f)
-    with open(os.path.join(save_folder,"history.txt"),'wb') as g:
-        history = "nof: " + str(opt.nof) + " model:" +str(opt.model) + " lr:" + str(opt.lr) + " neurons: " + str(opt.n1) + " " + str(opt.n2) + " " + str(opt.n3) + " kernel:" + str(3) + " norm data: " + str(opt.norm_method)
-        pickle.dump(history,g)
-
-else:
-    testloader = DataLoader(datasets,batch_size = 1, num_workers = opt.nb_workers)
+if opt.mode == "cross":
+  cross_validation()
+elif opt.mode == "train":
+  train()
+  
