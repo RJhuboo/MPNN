9a008dd3462df022b5d65b608ec5f175ef1d4b1d
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-03-31 12:49:23 +0200

    Dataloader finitive edition

--- dataloader.py
+++ dataloader.py
@@ -18,11 +18,12 @@ def normalization(csv_file,mode,indices):
     return scaler
 
 class Datasets(Dataset):
-    def __init__(self, csv_file, image_dir, opt, transform=None):
+    def __init__(self, csv_file, image_dir, opt, indices,transform=None):
         self.opt = opt
         self.image_dir = image_dir
         self.labels = csv_file
         self.transform = transform
+        self.indices = indices
     def __len__(self):
         return len(self.labels)
     def __getitem__(self, idx):
@@ -32,22 +33,22 @@ class Datasets(Dataset):
         image = io.imread(img_name) # Loading Image
         image = image / 255.0 # Normalizing [0;1]
         image = image.astype('float32') # Converting images to float32
-        #if self.opt.norm_method== "L2":
-        #    lab = preprocessing.normalize(self.labels.iloc[:,1:],axis=0)
-        #elif self.opt.norm_method == "L1":
-        #    lab = preprocessing.normalize(self.labels.iloc[:,1:],norm='l1',axis=0)
-        #elif self.opt.norm_method == "minmax":
-        #    scaler = preprocessing.MinMaxScaler()
-        #    scaler.fit(self.labels.iloc[:,1:])
-        #    lab = scaler.transform(self.labels.iloc[:,1:])
-        #elif self.opt.norm_method == "standardization":
-        #    scaler = preprocessing.StandardScaler()
-        #    scaler.fit(self.labels.iloc[:,1:])
-        #    lab = scaler.transform(self.labels.iloc[:,1:])
-        #lab = pd.DataFrame(self.labels)
-        #lab.insert(0,"File name", self.labels.iloc[:,0], True)
-        #lab.columns = self.labels.columns
-        labels = self.labels.iloc[idx,1:] # Takes all corresponding labels
+        if self.opt.norm_method== "L2":
+            lab = preprocessing.normalize(self.labels.iloc[:,1:],axis=0)
+        elif self.opt.norm_method == "L1":
+            lab = preprocessing.normalize(self.labels.iloc[:,1:],norm='l1',axis=0)
+        elif self.opt.norm_method == "minmax":
+            scaler = preprocessing.MinMaxScaler()
+            scaler.fit(self.labels.iloc[self.indices])
+            lab = scaler.transform(self.labels.iloc[:,1:])
+        elif self.opt.norm_method == "standardization":
+            scaler = preprocessing.StandardScaler()
+            scaler.fit(self.labels.iloc[self.indices])
+            lab = scaler.transform(self.labels.iloc[:,1:])
+        lab = pd.DataFrame(self.labels)
+        lab.insert(0,"File name", self.labels.iloc[:,0], True)
+        lab.columns = self.labels.columns
+        labels = lab.iloc[idx,1:] # Takes all corresponding labels
         labels = np.array([labels]) 
         labels = labels.astype('float32')
         #sample = {'image': image, 'label': labels}
