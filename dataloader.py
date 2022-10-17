d99c46229e14a69ff6c6f8663ff50d92fcf5834a
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-10-17 12:57:28 +0200

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -57,20 +57,27 @@ class Datasets(Dataset):
         return {'image': image, 'label': labels, 'ID': lab.iloc[idx,0]}
 
 class Test_Datasets(Dataset):
-    def __init__(self, image_dir, transform=None):
+    def __init__(self, csv_file, image_dir, opt, scaler, transform=None):
         self.image_dir = image_dir
         self.transform = transform
+        self.scaler = scaler
     def __len__(self):
         return len(self.labels)
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
+          if torch.is_tensor(idx):
             idx = idx.tolist()
-        image_name = os.listdir(self.image_dir)
-        img_name = os.path.join(self.image_dir,image_name[idx])
+        img_name = os.path.join(self.image_dir, str(self.labels.iloc[idx,0])[:-4] + ".png")
         image = io.imread(img_name) # Loading Image
         image = image / 255.0 # Normalizing [0;1]
         image = image.astype('float32') # Converting images to float32
-        #sample = {'image': image}
-        #if self.transform:
-        #    sample = self.transform(sample)
-        return image
+        lab = scaler.transform(self.labels.iloc[:,1:])
+        lab = pd.DataFrame(lab)
+        lab.insert(0,"File name", self.labels.iloc[:,0], True)
+        lab.columns = self.labels.columns
+        labels = lab.iloc[idx,1:] # Takes all corresponding labels
+        labels = np.array([labels]) 
+        labels = labels.astype('float32')
+        if self.transform:
+            sample = self.transform(sample)
+        return {'image': image, 'label': labels, 'ID': lab.iloc[idx,0]}
+   
