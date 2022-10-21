095cbf502874d600ca939ab8efd3b722a2a07844
Rehan JHUBOO <rehan.jhuboo@univ-st-etienne.fr>
2022-10-21 11:02:36 +0200

    new

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ else:
 ''' Options '''
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--label_dir", default = "./Test_Label_6p.csv", help = "path to label csv file")
-parser.add_argument("--image_dir", default = "/gpfsstore/rech/tvs/uki75tv/MOUSE_BPNN/HR/Test_Label_trab", help = "path to image directory")
+parser.add_argument("--label_dir", default = "./Label_6p.csv", help = "path to label csv file")
+parser.add_argument("--image_dir", default = "/gpfsstore/rech/tvs/uki75tv/MOUSE_BPNN/HR/Train_Label_trab", help = "path to image directory")
 parser.add_argument("--train_cross", default = "./cross_output.pkl", help = "filename of the output of the cross validation")
 parser.add_argument("--batch_size", type=int, default = 16, help = "number of batch")
 parser.add_argument("--model", default = "MultiNet", help="Choose model : Unet or ConvNet") 
@@ -85,7 +85,7 @@ def train():
     score_mse_v = []
     # defining data
     index = range(NB_DATA)
-    split = train_test_split(index,test_size = 0.2,random_state=1)
+    split = train_test_split(index,test_size = 0.2,shuffle=False)
     datasets = dataloader.Datasets(csv_file = opt.label_dir, image_dir = opt.image_dir, opt=opt, indices = split[0]) # Create dataset
     print("start training")
     trainloader = DataLoader(datasets, batch_size = opt.batch_size, sampler = split[0], num_workers = opt.nb_workers )
