bae337227a00b121c73985ec4e811f64a5d03108
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-10-24 10:24:04 +0200

    New model

--- Model.py
+++ Model.py
@@ -22,136 +22,6 @@ class NeuralNet(nn.Module):
         x = self.fc4(x)
         return x
 
-class Bottleneck(nn.Module):
-    expansion = 4
-    def __init__(self, in_channels, out_channels, i_downsample=None, stride=1):
-        super(Bottleneck, self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
-        self.batch_norm1 = nn.BatchNorm2d(out_channels)
-        
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
-        self.batch_norm2 = nn.BatchNorm2d(out_channels)
-        
-        self.conv3 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=1, stride=1, padding=0)
-        self.batch_norm3 = nn.BatchNorm2d(out_channels*self.expansion)
-        
-        self.i_downsample = i_downsample
-        self.stride = stride
-        self.relu = nn.ReLU()
-        
-    def forward(self, x):
-        identity = x.clone()
-        x = self.relu(self.batch_norm1(self.conv1(x)))
-        
-        x = self.relu(self.batch_norm2(self.conv2(x)))
-        
-        x = self.conv3(x)
-        x = self.batch_norm3(x)
-        
-        #downsample if needed
-        if self.i_downsample is not None:
-            identity = self.i_downsample(identity)
-        #add identity
-        x+=identity
-        x=self.relu(x)
-        
-        return x
-
-class Block(nn.Module):
-    expansion = 1
-    def __init__(self, in_channels, out_channels, i_downsample=None, stride=1):
-        super(Block, self).__init__()
-       
-
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride, bias=False)
-        self.batch_norm1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=stride, bias=False)
-        self.batch_norm2 = nn.BatchNorm2d(out_channels)
-
-        self.i_downsample = i_downsample
-        self.stride = stride
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-      identity = x.clone()
-
-      x = self.relu(self.batch_norm2(self.conv1(x)))
-      x = self.batch_norm2(self.conv2(x))
-
-      if self.i_downsample is not None:
-          identity = self.i_downsample(identity)
-      print(x.shape)
-      print(identity.shape)
-      x += identity
-      x = self.relu(x)
-      return x
-
-
-        
-        
-class ResNet(nn.Module):
-    def __init__(self, ResBlock, layer_list, num_classes, num_channels=1):
-        super(ResNet, self).__init__()
-        self.in_channels = 64
-        
-        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.batch_norm1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU()
-        self.max_pool = nn.MaxPool2d(kernel_size = 3, stride=2, padding=1)
-        
-        self.layer1 = self._make_layer(ResBlock, layer_list[0], planes=64)
-        self.layer2 = self._make_layer(ResBlock, layer_list[1], planes=128, stride=2)
-        self.layer3 = self._make_layer(ResBlock, layer_list[2], planes=256, stride=2)
-        self.layer4 = self._make_layer(ResBlock, layer_list[3], planes=512, stride=2)
-        
-        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.fc = nn.Linear(512*ResBlock.expansion, num_classes)
-        
-    def forward(self, x):
-        x = self.relu(self.batch_norm1(self.conv1(x)))
-        x = self.max_pool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        
-        x = self.avgpool(x)
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc(x)
-        
-        return x
-        
-    def _make_layer(self, ResBlock, blocks, planes, stride=1):
-        ii_downsample = None
-        layers = []
-        
-        if stride != 1 or self.in_channels != planes*ResBlock.expansion:
-            ii_downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, planes*ResBlock.expansion, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(planes*ResBlock.expansion)
-            )
-            
-        layers.append(ResBlock(self.in_channels, planes, i_downsample=ii_downsample, stride=stride))
-        self.in_channels = planes*ResBlock.expansion
-        
-        for i in range(blocks-1):
-            layers.append(ResBlock(self.in_channels, planes))
-            
-        return nn.Sequential(*layers)
-
-        
-        
-def ResNet50(num_classes, channels=1):
-    return ResNet(Bottleneck, [3,4,6,3], num_classes, channels)
-    
-def ResNet101(num_classes, channels=1):
-    return ResNet(Bottleneck, [3,4,23,3], num_classes, channels)
-
-def ResNet152(num_classes, channels=1):
-    return ResNet(Bottleneck, [3,8,36,3], num_classes, channels)
- 
 ## 3 CNN model ##
 class ConvNet(nn.Module):
     def __init__(self,features,out_channels,n1=240,n2=120,n3=60,k1=3,k2=3,k3=3):
@@ -177,135 +47,142 @@ class ConvNet(nn.Module):
         return x 
        
 ## Multitasking ##
-class MultiNet(nn.Module):
-    def __init__(self, features,out_channels,n1=240,n2=120,n3=60,k1=3,k2=3,k3=3):
-        super(MultiNet,self).__init__()
-        # initialize CNN layers 
-        self.conv1 = nn.Conv2d(1,features,kernel_size = k1,stride = 1, padding = 1)
-        self.conv2 = nn.Conv2d(features,features*2, kernel_size = k2, stride = 1, padding = 1)
-        self.conv3 = nn.Conv2d(features*2,64, kernel_size = k3, stride = 1, padding = 1)
-        self.pool = nn.MaxPool2d(2,2)
-        # initialize NN layers
-        self.neural_p1 = NeuralNet(n1,n2,n3,1)
-        self.neural_p2 = NeuralNet(n1,n2,n3,1)
-        self.neural_p3 = NeuralNet(n1,n2,n3,1)
-        self.neural_p4 = NeuralNet(n1,n2,n3,1)
-        self.neural_p5 = NeuralNet(n1,n2,n3,1)
+class FFNN(nn.Module):
+    """Simple FF network with multiple outputs.
+    """
+    def __init__(
+        self,
+        input_size,
+        hidden_size,
+        n_hidden,
+        n_outputs,
+        dropout_rate=.1,
+    ):
+        """
+        :param input_size: input size
+        :param hidden_size: common hidden size for all layers
+        :param n_hidden: number of hidden layers
+        :param n_outputs: number of outputs
+        :param dropout_rate: dropout rate
+        """
+        super().__init__()
+        assert 0 <= dropout_rate < 1
+        self.input_size = input_size
+
+        h_sizes = [self.input_size] + [hidden_size for _ in range(n_hidden)] + [n_outputs]
+
+        self.hidden = nn.ModuleList()
+        for k in range(len(h_sizes) - 1):
+            self.hidden.append(
+                nn.Linear(
+                    h_sizes[k],
+                    h_sizes[k + 1]
+                )
+            )
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=dropout_rate)
+
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        p1 = self.neural_p1(x)
-        p2 = self.neural_p2(x)
-        p3 = self.neural_p3(x)
-        p4 = self.neural_p4(x)
-        p5 = self.neural_p5(x)
-        return [p1,p2,p3,p4,p5]
+
+        for layer in self.hidden[:-1]:
+            x = layer(x)
+            x = self.relu(x)
+            x = self.dropout(x)
+
+        return self.hidden[-1](x)
     
-## UNET model ##
-class UNet(nn.Module):
+class TaskIndependentLayers(nn.Module):
+    """NN for MTL with hard parameter sharing
+    """
 
-    def __init__(self, in_channels=1, out_channels=1,nb_label=14, n1=240,n2=120,n3=60, init_features=64):
-        super(UNet, self).__init__()
+    def __init__(
+            self,
+            input_size,
+            hidden_size,
+            n_hidden,
+            n_outputs,
+            dropout_rate=.1,
+    ):
 
-        features = init_features
-        self.encoder1 = UNet._block(in_channels, features, name="enc1")
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder2 = UNet._block(features, features * 2, name="enc2")
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder3 = UNet._block(features * 2, features * 4, name="enc3")
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder4 = UNet._block(features * 4, features * 8, name="enc4")
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        super().__init__()
 
-        self.bottleneck = UNet._block(features * 8, features * 16, name="bottleneck")
+        self.n_outputs = n_outputs
+        self.task_nets = nn.ModuleList()
+        for _ in range(n_outputs):
+            self.task_nets.append(
+                FFNN(
+                    input_size=input_size,
+                    hidden_size=hidden_size,
+                    n_hidden=n_hidden,
+                    n_outputs=1,
+                    dropout_rate=dropout_rate,
+                )
+            )
 
-        self.upconv4 = nn.ConvTranspose2d(
-            features * 16, features * 8, kernel_size=2, stride=2
-        )
-        self.decoder4 = UNet._block((features * 8) * 2, features * 8, name="dec4")
-        self.upconv3 = nn.ConvTranspose2d(
-            features * 8, features * 4, kernel_size=2, stride=2
-        )
-        self.decoder3 = UNet._block((features * 4) * 2, features * 4, name="dec3")
-        self.upconv2 = nn.ConvTranspose2d(
-            features * 4, features * 2, kernel_size=2, stride=2
-        )
-        self.decoder2 = UNet._block((features * 2) * 2, features * 2, name="dec2")
-        self.upconv1 = nn.ConvTranspose2d(
-            features * 2, features, kernel_size=2, stride=2
-        )
-        self.decoder1 = UNet._block(features * 2, features, name="dec1")
-
-
-        #self.conv = nn.Conv2d(
-        #    in_channels=features, out_channels=out_channels, kernel_size=1
-        #)
-
-        self.conv = nn.Conv2d(in_channels=features, out_channels=features*3, kernel_size=3,stride=1,padding=1)
-        self.pool5 = nn.MaxPool2d(kernel_size=2,stride=2)
-        self.conv2 = nn.Conv2d(in_channels=features*3, out_channels=features*2,kernel_size=3,stride=1,padding=1)
-        self.pool6 = nn.MaxPool2d(kernel_size=2,stride=2)
-        self.conv3 = nn.Conv2d(features*2,64,kernel_size=3,stride=1,padding=1)
-        self.pool7 = nn.MaxPool2d(kernel_size=2,stride=2)
-        
-        self.neural = NeuralNet(n1,n2,n3,nb_label)
-        
     def forward(self, x):
-        enc1 = self.encoder1(x)
-        enc2 = self.encoder2(self.pool1(enc1))
-        enc3 = self.encoder3(self.pool2(enc2))
-        enc4 = self.encoder4(self.pool3(enc3))
+ 
+        return torch.cat(
+            tuple(task_model(x) for task_model in self.task_nets),
+            dim=1
+        )
+class HardSharing(nn.Module):
+    """FFNN with hard parameter sharing
+    """
 
-        bottleneck = self.bottleneck(self.pool4(enc4))
+    def __init__(
+        self,
+        input_size,
+        hidden_size,
+        n_hidden,
+        n_outputs,
+        n_task_specific_layers=0,
+        task_specific_hidden_size=None,
+        dropout_rate=.1,
+    ):
 
-        dec4 = self.upconv4(bottleneck)
-        dec4 = torch.cat((dec4, enc4), dim=1)
-        dec4 = self.decoder4(dec4)
-        dec3 = self.upconv3(dec4)
-        dec3 = torch.cat((dec3, enc3), dim=1)
-        dec3 = self.decoder3(dec3)
-        dec2 = self.upconv2(dec3)
-        dec2 = torch.cat((dec2, enc2), dim=1)
-        dec2 = self.decoder2(dec2)
-        dec1 = self.upconv1(dec2)
-        dec1 = torch.cat((dec1, enc1), dim=1)
-        dec1 = self.decoder1(dec1)
-        x = self.pool5(F.relu(self.conv(dec1)))
-        x = self.pool6(F.relu(self.conv2(x)))
-        x = self.pool7(F.relu(self.conv3(x)))
-        return self.neural(x)
+        super().__init__()
+        if task_specific_hidden_size is None:
+            task_specific_hidden_size = hidden_size
 
-    @staticmethod
-    def _block(in_channels, features, name):
-        return nn.Sequential(
-            OrderedDict(
-                [
-                    (
-                        name + "conv1",
-                        nn.Conv2d(
-                            in_channels=in_channels,
-                            out_channels=features,
-                            kernel_size=3,
-                            padding=1,
-                            bias=False,
-                        ),
-                    ),
-                    (name + "norm1", nn.BatchNorm2d(num_features=features)),
-                    (name + "relu1", nn.ReLU(inplace=True)),
-                    (
-                        name + "conv2",
-                        nn.Conv2d(
-                            in_channels=features,
-                            out_channels=features,
-                            kernel_size=3,
-                            padding=1,
-                            bias=False,
-                        ),
-                    ),
-                    (name + "norm2", nn.BatchNorm2d(num_features=features)),
-                    (name + "relu2", nn.ReLU(inplace=True)),
-                ]
+        self.model = nn.Sequential()
+
+        self.model.add_module(
+            'hard_sharing',
+            FFNN(
+                input_size=input_size,
+                hidden_size=hidden_size,
+                n_hidden=n_hidden,
+                n_outputs=hidden_size,
+                dropout_rate=dropout_rate
             )
         )
+
+        if n_task_specific_layers > 0:
+            # if n_task_specific_layers == 0 than the task specific mapping is linear and
+            # constructed as the product of last layer is the 'hard_sharing' and the linear layer
+            # in 'task_specific', with no activation or dropout
+            self.model.add_module('relu', nn.ReLU())
+            self.model.add_module('dropout', nn.Dropout(p=dropout_rate))
+
+        self.model.add_module(
+            'task_specific',
+            TaskIndependentLayers(
+                input_size=hidden_size,
+                hidden_size=task_specific_hidden_size,
+                n_hidden=n_task_specific_layers,
+                n_outputs=n_outputs,
+                dropout_rate=dropout_rate
+            )
+        )
+        
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            module.weight.data.normal_(mean=0.0, std=config.initializer_range)
+            if module.bias is not None:
+                module.bias.data.zero_()
+                
+    def forward(self, x):
+        x = torch.flatten(x,1) 
+        return self.model(x)
     
