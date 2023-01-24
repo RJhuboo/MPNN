f604899b5aa8dba16c5fbbcc6c1842fee07d7042
Rehan JHUBOO <rehan.jhuboo@univ-st-etienne.fr>
2023-01-24 19:16:20 +0100

    marche pas

--- dataloader.py
+++ dataloader.py
@@ -2,8 +2,6 @@ import torch
 import os
 import numpy as np
 import pandas as pd
-import torchvision.transforms.functional as TF
-import random
 from torch.utils.data import Dataset, DataLoader
 from skimage import io,transform
 from torchvision import transforms, utils
@@ -21,15 +19,14 @@ def normalization(csv_file,mode,indices):
     return scaler
 
 class Datasets(Dataset):
-    def __init__(self, csv_file, image_dir, mask_dir, scaler, opt, transforms, upsample = False):
+    def __init__(self, csv_file, image_dir, mask_dir, scaler, opt,transform=None):
         self.opt = opt
         self.image_dir = image_dir
         self.labels = pd.read_csv(csv_file)
         self.mask_dir = mask_dir
+        self.transform = transform
         self.scaler = scaler
         self.mask_use = True
-        self.upsample = upsample
-        self.transforms=transforms
     def __len__(self):
         return len(self.labels)
     def __getitem__(self, idx):
@@ -43,13 +40,14 @@ class Datasets(Dataset):
             #image = (image>0.5)
             print("image unique",np.unique(image))
             mask_name = os.path.join(self.mask_dir,(str(self.labels.iloc[idx,0]).replace(".tif",".png")).replace("im_lr","im"))
+
         if self.mask_use == True:
             mask_name
             mask = io.imread(mask_name)
             mask = transform.rescale(mask, 1/8, anti_aliasing=False)
             mask = mask / 255.0 # Normalizing [0;1]
             mask = mask.astype('float32') # Converting images to float32
-            #image = image / 255.0 # Normalizing [0;1]
+            image = image / 255.0 # Normalizing [0;1]
             image = image.astype('float32') # Converting images to float32
         else:
             image = image / 255.0 # Normalizing [0;1]
@@ -63,7 +61,7 @@ class Datasets(Dataset):
         labels = np.array([labels]) 
         #print(np.shape(labels))
         #labels = labels.reshape(-1,1)
-        labels = labels.astype('float32')
+        labels = labels.astype('float32') 
         p = random.random()
         rot = random.randint(-45,45)
         transform_list = []
@@ -84,3 +82,22 @@ class Datasets(Dataset):
         #    if self.mask_use == True:
         #        mask = self.transform(mask)
         return {'image': image, 'mask': mask, 'label': labels, 'ID': lab.iloc[idx,0]}
+
+class Test_Datasets(Dataset):
+    def __init__(self, image_dir, transform=None):
+        self.image_dir = image_dir
+        self.transform = transform
+    def __len__(self):
+        return len(self.labels)
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        image_name = os.listdir(self.image_dir)
+        img_name = os.path.join(self.image_dir,image_name[idx])
+        image = io.imread(img_name) # Loading Image
+        image = image / 255.0 # Normalizing [0;1]
+        image = image.astype('float32') # Converting images to float32
+        #sample = {'image': image}
+        #if self.transform:
+        #    sample = self.transform(sample)
+        return image
