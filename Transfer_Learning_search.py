33e2ddbe1812f1ef9a2d1e6a1ac28145197be5d6
Rehan Jhuboo <rehan.jhuboo@univ-st-etienne.fr>
2023-05-26 09:15:27 +0200

    change pickle file name

--- Transfer_Learning_search.py
+++ Transfer_Learning_search.py
@@ -397,5 +397,5 @@ else:
 # create a study on optuna for hyperparameter tuning
 study.optimize(objective,n_trials=12) # n_trials is the number of experiments to run
 # Save the results of the study to a pickle file
-with open("./cross_7p_augment_lrhr.pkl","wb") as f:
+with open("./cross_7p_transferlearning.pkl","wb") as f:
     pickle.dump(study,f)
