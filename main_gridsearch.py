8df14e03c71611965c962dcc679737a7b58a285c
Rehan Jhuboo <rehan.jhuboo@univ-st-etienne.fr>
2023-07-10 11:14:34 +0200

    tensorboard

--- main_gridsearch.py
+++ main_gridsearch.py
@@ -7,6 +7,7 @@ import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torch.nn import MSELoss,L1Loss
+from torch.utils.tensorboard import SummaryWriter
 from torch.optim import Adam
 import torchvision.transforms.functional as TF
 import torchvision.transforms as transforms
