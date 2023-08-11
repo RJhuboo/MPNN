f8b7c3e7d7492d827a14bbbd9314c56a46112919
Rehan Jhuboo <rehan.jhuboo@univ-st-etienne.fr>
2023-08-11 16:35:07 +0200

    some changes

--- dataloader.py
+++ dataloader.py
@@ -46,8 +46,8 @@ class Datasets(Dataset):
         if self.mask_use == True:
             mask_name
             mask = io.imread(mask_name)
-            mask = transform.rescale(mask, 1/8, anti_aliasing=False)
-            mask = mask / 255.0 # Normalizing [0;1]
+            mask = (transform.rescale(mask, 1/8, anti_aliasing=False)>0.1)*1.
+            #mask = mask / 255.0 # Normalizing [0;1]
             mask = mask.astype('float32') # Converting images to float32
             image = image / 255.0 # Normalizing [0;1]
             image = image.astype('float32') # Converting images to float32
