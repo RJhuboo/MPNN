39c1ec84f9a708c9e7243c063ffbb3bc2d2368bc
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2024-01-16 12:09:41 +0100

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -77,32 +77,4 @@ class Datasets(Dataset):
         image,mask=TF.to_tensor(image),TF.to_tensor(mask)
         
         return {'image': image, 'mask': mask, 'label': labels, 'ID':  self.labels.iloc[idx,0]}
-
-class Test_Datasets(Dataset):
-    def __init__(self, csv_file, image_dir, opt, scaler, transform=None):
-        self.opt = opt
-        self.image_dir = image_dir
-        self.labels = pd.read_csv(csv_file)
-        self.transform = transform
-        self.scaler = scaler
-    def __len__(self):
-        return len(self.labels)
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        img_name = os.path.join(self.image_dir, str(self.labels.iloc[idx,0])[:-4] + ".png")
-        print(img_name)
-        image = io.imread(img_name) # Loading Image
-        image = image / 255.0 # Normalizing [0;1]
-        image = image.astype('float32') # Converting images to float32
-        lab = self.scaler.transform(self.labels.iloc[:,1:])
-        lab = pd.DataFrame(lab)
-        lab.insert(0,"File name", self.labels.iloc[:,0], True)
-        lab.columns = self.labels.columns
-        labels = lab.iloc[idx,1:] # Takes all corresponding labels
-        labels = np.array([labels]) 
-        labels = labels.astype('float32')
-        if self.transform:
-            sample = self.transform(sample)
-        return {'image': image, 'label': labels, 'ID': lab.iloc[idx,0]}
    
